86ccc22ace33ffe463da99974d70672b279d0c52
Fernando Mejia <fernandoemejia092018@gmail.com>
2020-02-05 13:52:40 -0500

    Completed module2

--- api/app.js
+++ api/app.js
@@ -1,7 +1,11 @@
 const express=require('express');
 const path=require('path');
 const app=express();
-
+const router= require('./src/router');
 const pathToIndex = path.resolve(__dirname ,'../client/index.html');
-app.use('/*', (request, response)=>{ response.sendFile(pathToIndex)})
+
+app.use('/',router);
+app.use(express.static(path.resolve(__dirname,'uploads')));
+app.use('/*', (request, response)=>{ response.sendFile(pathToIndex)});
+
 module.exports=app
